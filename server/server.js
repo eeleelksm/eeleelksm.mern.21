49765525e9ff2c1cf258d473fe978133d1b7c879
Keshia-Lee Martin <keshialeemartin@gmail.com>
2022-01-20 20:45:45 -0500

    server.js

--- server/server.js
+++ server/server.js
@@ -35,13 +35,6 @@ console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 // Initialize the Apollo server
 startServer();
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../client/build")));
-}
-
-app.use(routes);
-
 db.once("open", () => {
 	app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
