3f0232677b4d979a6e90835f3588ddb12719539e
Keshia-Lee Martin <keshialeemartin@gmail.com>
2022-01-21 11:25:15 -0500

    completed resolvers for user and books

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,9 @@
 const { User, Book } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
 	Query: {
-		// get a single user by username
 		me: async (parent, args, context) => {
 			if (context.user) {
 				const userData = await User.findOne({ _id: context.user._id })
@@ -11,7 +12,58 @@ const resolvers = {
 
 				return userData;
 			}
-			// throw new AuthenticationError("Not logged in");
+			throw new AuthenticationError("Not logged in");
+		},
+		// get a single user by username
+		user: async (parent, { username }) => {
+			return User.findOne({ username }).select("-__v -password").populate("books");
+		},
+	},
+
+	Mutation: {
+		addUser: async (parent, args) => {
+			const user = await User.create(args);
+			const token = signToken(user);
+			return { token, user };
+		},
+
+		login: async (parent, { email, password }) => {
+			const user = await User.findOne({ email });
+
+			if (!user) {
+				throw new AuthenticationError("Incorrect credentials");
+			}
+
+			const correctPw = await user.isCorrectPassword(password);
+			if (!correctPw) {
+				throw new AuthenticationError("Incorrect credentials");
+			}
+			const token = signToken(user);
+			return { token, user };
+		},
+
+		saveBook: async (parent, { savedBooks }, context) => {
+			if (context.user) {
+				const updatedUser = await User.findOneAndUpdate(
+					{ _id: user._id },
+					{ $addToSet: { savedBooks: body } },
+					{ new: true, runValidators: true }
+				);
+				return updatedUser;
+			}
+			throw new AuthenticationError("You need to login.");
+		},
+
+		removeBook: async (parent, { bookId }, context) => {
+			if (context.user) {
+				const updatedUser = await User.findOneAndUpdate(
+					{ _id: context.user._id },
+					{ $pull: { savedBooks: { bookId: params.bookId } } },
+					{ new: true }
+				).populate("books");
+				return updatedUser;
+			}
+			throw new AuthenticationError("");
 		},
 	},
 };
