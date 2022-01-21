565530134c2e913d8e6a706cdffb4b10ef4644e0
Keshia-Lee Martin <keshialeemartin@gmail.com>
2022-01-20 19:57:14 -0500

    created schema folder - set up typeDefs file partially

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,13 @@
+// import the gql tagged template function
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql``;
+
+module.exports = typeDefs;
+
+// export
+// query type - me: returns a User type
+// mutation type
+// login - accepts an email and password as parameters, returns an Auth type
+// addUser - accepts a username, email, password as parameters, returns an Auth type
+// saveBook -
