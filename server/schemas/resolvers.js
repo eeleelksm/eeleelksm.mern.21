390fa72a038b794d2aed90665a272674bb7f3a84
Keshia-Lee Martin <keshialeemartin@gmail.com>
2022-01-21 10:56:06 -0500

    completed typeDef

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,8 +2,16 @@ const { User, Book } = require("../models");
 
 const resolvers = {
 	Query: {
-		books: async () => {
-			return Book.find();
+		// get a single user by username
+		me: async (parent, args, context) => {
+			if (context.user) {
+				const userData = await User.findOne({ _id: context.user._id })
+					.select("-__v -password")
+					.populate("books");
+
+				return userData;
+			}
+			// throw new AuthenticationError("Not logged in");
 		},
 	},
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,15 +3,47 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
 	type Query {
-		books: [Book]
+		me: User
+	}
+
+	type User {
+		_id: ID
+		username: String!
+		email: String!
+		bookCount: Int
+		savedBooks: [Book]
 	}
 
 	type Book {
-		bookId: ID
+		bookId: String!
+		authors: [String]
+		description: String
+		title: String!
+		image: String
+		link: String
+	}
+
+	input bookInput {
+    bookId: String
 		authors: [String]
 		description: String
 		title: String
+		image: String
+		link: String
 	}
+	}
+
+	type Mutation {
+		login(email: String!, password: String!): Auth
+		addUser(username: String!, email: String!, password: String!): Auth
+		saveBook(bookData: bookInput!): User
+		removeBook(bookId: ID!): User
+	}
+
+  type Auth {
+    token: ID!
+    user: User
+  }
 `;
 
 module.exports = typeDefs;
