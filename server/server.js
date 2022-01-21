28e27b9aa714d3cd1955cf0249773ae4d7d3130c
Keshia-Lee Martin <keshialeemartin@gmail.com>
2022-01-20 20:14:59 -0500

    working on server js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,36 @@
 const express = require("express");
+const path = require("path");
 // import Apollo Server
 const { ApolloServer } = require("apollo-server-express");
 
-const path = require("path");
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require("apollo-server-express");
 const db = require("./config/connection");
-const routes = require("./routes");
+const { authMiddleware } = require("./utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// create a new Apollo server and pass in schema data
+const startServer = async () => {
+	const server = new ApolloServer({
+		typeDefs,
+		resolvers,
+		// context: authMiddleware
+	});
+};
+
+// start Apollo server
+await server.start();
+
+// integrate our Apollo server with the Express application as middlewaaare
+server.applyMiddleware({ app });
+
+// log where we go to test our GQL API
+console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
