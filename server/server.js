d5241e2354567d0a0d9b9c5cc087712ab59cb4f3
Keshia-Lee Martin <keshialeemartin@gmail.com>
2022-01-20 20:36:10 -0500

    working server.js

--- server/server.js
+++ server/server.js
@@ -32,6 +32,9 @@ server.applyMiddleware({ app });
 // log where we go to test our GQL API
 console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 
+// Initialize the Apollo server
+startServer();
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "../client/build")));
